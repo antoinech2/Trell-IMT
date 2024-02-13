38163ee9db80f344d62005c7fc0f846697f45cc7
Emmanuelle Sojka <emmanuelle.sojka@free.fr>
2024-02-13 16:07:44 +0100

    login page first attempt

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+@app.route('/register_login')
+def static_resources_view():
+    return flask.render_template("register_login.html.jinja2")
+
 
 
 if __name__ == '__main__':
