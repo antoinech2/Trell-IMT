a7b312b99ffcdd5224d4be2f5afd1e12d7da5270
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2024-02-21 21:30:02 +0100

    Changed home page nagivation No more need to check if the user is connected

--- src/routes/home.py
+++ src/routes/home.py
@@ -35,27 +35,21 @@ def get_user_new_project():
 
 
 @app.route('/')
-@login_required
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Page d''accueil'
 
 
 @app.route('/home')
+@login_required
 def home_view():
     return flask.render_template("header.html.jinja2",
-                                 username = get_user_name(),
-                                 user_logo = get_user_logo(),
-                                 user_profile = get_user_profile(),
-                                 user_project = get_user_project(),
-                                 user_new_project = get_user_new_project(),
+                                 user = current_user,
+                                 user_logo = "./static/img/logo_user.jpg",
                                  page_template = "task.html.jinja2")
 
 @app.route('/contact')
 def contact_view():
     return flask.render_template("header.html.jinja2",
-                                 username = get_user_name(),
-                                 user_logo = get_user_logo(),
-                                 user_profile = get_user_profile(),
-                                 user_project = get_user_project(),
-                                 user_new_project = get_user_new_project(),
+                                 user = current_user,
+                                 user_logo = "./static/img/logo_user.jpg",
                                  page_template = "contact.html.jinja2")
