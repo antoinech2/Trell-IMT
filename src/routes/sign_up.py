9cab0c8989246332c27e7b5fe5ba4ea470162874
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2024-02-21 20:51:50 +0100

    Added password validation

--- src/routes/sign_up.py
+++ src/routes/sign_up.py
@@ -1,3 +1,5 @@
+import re
+
 import bcrypt
 import flask
 
@@ -8,11 +10,39 @@ from src.database.models import *
 @app.route('/sign_up', methods=["GET", "POST"])
 def sign_up_view():
     form = flask.request.form
-    if form:
+    valid_form, errors = validate_sign_up_form(form)
+    if valid_form:
         new_user = User(email=form.get("email"), password=bcrypt.hashpw(form.get("password").encode('utf-8'), salt),
                         first_name=form.get("first_name"), last_name=form.get("last_name"), type=form.get("user_type"))
         db.session.add(new_user)
         db.session.commit()
         return flask.render_template("sign_up_success.html.jinja2")
     else:
-        return flask.render_template("sign_up.html.jinja2")
+        return flask.render_template("sign_up.html.jinja2", errors=errors, form=form)
+
+
+def validate_sign_up_form(form):
+    result = True
+    errors = []
+    if not form:
+        result = False
+        return result, errors
+    if form.get("confirmpassword") != form.get("password"):
+        result = False
+        errors.append("Password confirmation does not match with original password")
+    if len(form.get("password")) < 7:
+        result = False
+        errors.append("Password must be at least 7 characters long")
+    if not any(char.isdigit() for char in form.get("password")):
+        result = False
+        errors.append("Password must contain at least one number")
+    if not any(char.isupper() for char in form.get("password")):
+        result = False
+        errors.append("Password must contain at least one uppercase letter")
+
+    special_chars = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+    if not special_chars.search(form.get("password")):
+        result = False
+        errors.append("Password must contain at least one special character")
+
+    return result, errors
