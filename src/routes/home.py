9dced1d891949096980e7fcb3f7a6472a2f1344b
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2024-02-27 09:12:56 +0100

    Fix ressource import

--- src/routes/home.py
+++ src/routes/home.py
@@ -4,26 +4,20 @@ import flask
 from app import app
 from src.database.models import *
 
+
 @app.route('/')
 def hello_world():  # put application's code here
     return flask.render_template("site_page.html.jinja2")
 
-@app.route('/test')
-def test():  # put application's code here
-    return flask.render_template("components/add_task.html.jinja2", user = current_user)
 
 @app.route('/home')
 @login_required
 def home_view():
-    return flask.render_template("header.html.jinja2",
-                                 user=current_user,
-                                 user_logo="./static/img/logo_user.jpg",
-                                 page_template="home_page.html.jinja2")
+    return flask.render_template("home_page.html.jinja2",
+                                 user=current_user)
 
 
 @app.route('/contact')
 def contact_view():
-    return flask.render_template("header.html.jinja2",
-                                 user=current_user,
-                                 user_logo="./static/img/logo_user.jpg",
-                                 page_template="contact.html.jinja2")
+    return flask.render_template("contact.html.jinja2",
+                                 user=current_user)
