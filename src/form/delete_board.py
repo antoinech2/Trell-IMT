b4f3d5482c771657df3b6665fe1b74ee7ad6a09f
Emmanuelle Sojka <emmanuelle.sojka@free.fr>
2024-03-05 14:50:47 +0100

    edit board + delete board ( with an error )

---
+++ src/form/delete_board.py
@@ -0,0 +1,20 @@
+import flask
+from flask import request, redirect
+from flask_login import login_required
+
+from app import app
+from src.database.database import db
+from src.database.models import Board
+
+
+@app.route('/delete_board', methods=["POST"])
+@login_required
+def delete_board_form():
+    form = flask.request.form
+    board_id = request.args.get('board_id')
+    if form and board_id:
+        board = Board.query.filter_by(id=board_id).first()
+        db.session.delete(board)
+        db.session.commit()
+
+    return redirect(request.referrer)
