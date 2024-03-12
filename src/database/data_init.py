e0fe161e48b573155558e2f6301441533718cc0c
Emmanuelle Sojka <emmanuelle.sojka@free.fr>
2024-03-12 14:39:45 +0100

    new colors

--- src/database/data_init.py
+++ src/database/data_init.py
@@ -5,9 +5,9 @@ from src.database.models import Etiquette
 def insert_initial_values(*args, **kwargs):
     db.session.add(Etiquette(type="priority", label="Critical", description="Most important task", color="f50a0a"))
     db.session.add(Etiquette(type="priority", label="High", description="Import task", color="f5830a"))
-    db.session.add(Etiquette(type="priority", label="Medium", description="Quite important task", color="f5e10a"))
-    db.session.add(Etiquette(type="priority", label="Low", description="Not important task", color="1af045"))
-    db.session.add(Etiquette(type="priority", label="Lowest", description="Lowest important task", color="2ef2df"))
+    db.session.add(Etiquette(type="priority", label="Medium", description="Quite important task", color="cfb300"))
+    db.session.add(Etiquette(type="priority", label="Low", description="Not important task", color="03ad31"))
+    db.session.add(Etiquette(type="priority", label="Lowest", description="Lowest important task", color="4aa8c2"))
     db.session.add(Etiquette(type="status", label="Not started", description="Planned but not started yet", color="6d6f8a"))
     db.session.add(Etiquette(type="status", label="In progress", description="Work in progress", color="424bc2"))
     db.session.add(Etiquette(type="status", label="Blocked", description="Blocked by other task", color="6e4343"))
