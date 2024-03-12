bbea8292b612cf999af90b7558c7c62edd4edc87
Nathan CLAEYS <nathan.claeys@imt-atlantique.net>
2024-03-12 15:57:17 +0100

    change the way to import get_task function

--- src/routes/board_dev.py
+++ src/routes/board_dev.py
@@ -5,9 +5,7 @@ from flask_login import login_required, current_user
 
 from src.database.models import Board, Category, Task
 
-import sys
-sys.path.append("src/helper")
-from get_task import*
+from src.helper.get_task import get_task
 
 @app.route('/board_developer', methods=["GET", "POST"])
 @login_required
@@ -15,10 +13,10 @@ def board_developer():
     form = flask.request.form
     project_name = ["project1", "project2", "project3", "project4"]
     task_name = ["task1", "task2", "task3"]
-    importance = ["importance1", "importance2"]
-    states = ["state1", "state2", "state3"]
+    importance = ["none","importance1", "importance2"]
+    states = ["none","state1", "state2", "state3"]
 
-    tasks = get_tasks_from_form(form)
+    tasks = get_tasks_from_form2(form)
 
 
     return flask.render_template("developer/Board_developer.html.jinja2", user=current_user,
@@ -39,3 +37,22 @@ def get_tasks_from_form (form):
         return tasks_data[0:2]
     else:
         return tasks_data
+
+def get_tasks_from_form2 (form):
+    param = {}
+    tasks_data = []
+    print (form.get("project_name"))
+    if form.get("project_name"):
+        param["Board.name"] = form.get("project_name")
+    if form.get("task_name"):
+        param["Task.name"] = form.get("task_name")
+    if form.get("importance") != "none":
+        param["Task.priority"] = form.get("importance")
+    if form.get("state") != "none" :
+        param["states"] = form.get("state")
+    if form.get("tasks_date"):
+        param["Task.date_expires"] = form.get("tasks_date")
+    tasks = Task.query.join(Category, Category.id == Task.category_id, Board, Category.board_id == Board.id).filter_by(param).all()
+    for task in tasks:
+        tasks_data.append(get_task(task))
+    return tasks_data
