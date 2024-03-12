30a2fbd34599e5b72ee9d52c23bf301a6308e192
Nathan CLAEYS <nathan.claeys@imt-atlantique.net>
2024-03-12 14:26:24 +0100

    change the transforming task type to dictionary part using the separate function from get_task

--- src/routes/board.py
+++ src/routes/board.py
@@ -9,6 +9,9 @@ from flask_login import login_required, current_user
 from src.database.database import db
 from src.database.models import Board, Category, Task, Step, Etiquette
 
+import sys
+sys.path.append("src/helper")
+from get_task import*
 
 @app.route('/board/<board_id>')
 @login_required
@@ -29,36 +32,5 @@ def board(board_id):
         tasks_data[-1]['tasks'] = []
         tasks = Task.query.filter_by(category_id=category.id).all()
         for task in tasks:
-            task_dict = task.__dict__
-
-            subtasks_done = Step.query.filter_by(task_id=task_dict["id"], status="1").count()
-            subtasks_not_done = Step.query.filter_by(task_id=task_dict["id"], status="0").count()
-            if subtasks_done + subtasks_not_done > 0:
-                task_dict['progress'] = round(subtasks_done / (subtasks_done + subtasks_not_done) * 100)
-            task_dict['subtasks_done'] = subtasks_done
-            task_dict['subtasks_total'] = subtasks_done + subtasks_not_done
-            if task_dict["date_expires"]:
-                expired = False
-                delta_date = task_dict["date_expires"] - datetime.now()
-                days, seconds = delta_date.days, delta_date.seconds
-                if days < 0:
-                    expired = True
-                    delta_date = datetime.now() - task_dict["date_expires"]
-                    days, seconds = delta_date.days, delta_date.seconds
-                hours = (days * 24 + seconds) // 3600
-                minutes = (abs(seconds) % 3600) // 60
-                if abs(days) > 0:
-                    message_data = "{} days".format(abs(days))
-                elif abs(hours) > 0:
-                    message_data = "{} hours".format(abs(hours))
-                else:
-                    message_data = "{} minutes".format(abs(minutes))
-                if expired:
-                    task_dict["expires_message"] = "Expired {} ago".format(message_data)
-                    task_dict["has_expired"] = True
-                else:
-                    task_dict["expires_message"] = "Expires in {}".format(message_data)
-                    task_dict["has_expired"] = False
-                task_dict["date_expires"] = task_dict["date_expires"].strftime("%Y-%m-%dT%H:%M")
-            tasks_data[-1]['tasks'].append(task_dict)
+            tasks_data[-1]['tasks'].append(get_task(task))
     return flask.render_template("project_manager/board_manager.html.jinja2", tasks_data=tasks_data, user=current_user, board=board, etiquette_data = etiquette_data)
