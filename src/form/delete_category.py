8b3c66bcd6e55e206a3bf2ac334dfa2ab2ca4d6f
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2024-03-15 22:03:26 +0100

    Fixed task deletion

--- src/form/delete_category.py
+++ src/form/delete_category.py
@@ -5,6 +5,7 @@ from flask_login import login_required
 from app import app
 from src.database.database import db
 from src.database.models import Category, Task
+from src.form.delete_task import delete_task
 
 
 @app.route('/delete_category', methods=["POST"])
@@ -20,5 +21,5 @@ def delete_category_form():
 def delete_category(category_id):
     cat = Category.query.filter_by(id=category_id).first()
     db.session.delete(cat)
-    db.session.query(Task).filter_by(category_id=category_id).delete()
-    db.session.commit()
+    for task in Task.query.filter_by(category_id=category_id).all():
+        delete_task(task)
--- src/form/delete_task.py
+++ src/form/delete_task.py
@@ -4,7 +4,7 @@ from flask_login import login_required
 
 from app import app
 from src.database.database import db
-from src.database.models import Task
+from src.database.models import Task, Step
 
 
 @app.route('/delete_task', methods=["POST"])
@@ -14,7 +14,15 @@ def remove_task_form():
     task_id = request.args.get('task_id')
     if form and task_id:
         task = Task.query.filter_by(id=task_id).first()
-        db.session.delete(task)
-        db.session.commit()
+        delete_task(task)
 
     return redirect(request.referrer)
+
+
+def delete_task(task):
+    task.etiquettes = []
+    task.users = []
+    db.session.commit()
+    db.session.query(Step).filter_by(task_id=task.id).delete()
+    db.session.delete(task)
+    db.session.commit()
