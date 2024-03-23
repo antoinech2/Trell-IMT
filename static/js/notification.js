3648d83103fcb89aea7ff9c7f73f3e0cee569e9a
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2024-03-23 19:53:55 +0100

    Fix notification badge update

--- static/js/notification.js
+++ static/js/notification.js
@@ -2,6 +2,7 @@ $(async function () {
     $("#notification_button").on('click', function () {
         try {
             fetch("/get_notifications").then(r => r.json()).then(r => {
+                $("#notification_number").text("0")
                 $("#read_notifications").empty()
                 $("#unread_notifications").empty()
                 r.forEach(notif => showNotif(notif))
@@ -20,6 +21,7 @@ $(async function () {
 })
 
 function showNotif(data) {
+    let notificationBadge = $("#notification_number")
     let newNotif = $(`<div class="notification"><strong>${data.title}</strong><em class="comment-right" title='${data.time}'>${data.time_message}</em><h6>${data.content}</h6><button class="mark_read"></button></div>`)
     if (data.read){
         newNotif.find(".mark_read").text("Mark as unread")
@@ -28,6 +30,7 @@ function showNotif(data) {
     else{
         newNotif.find(".mark_read").text("Mark as read")
         $("#unread_notifications").append(newNotif)
+        notificationBadge.text(parseInt(notificationBadge.text())+1)
     }
     newNotif.find(".mark_read").on('click', function () {
         try {
@@ -36,6 +39,9 @@ function showNotif(data) {
             }).then(r => r.json()).then(r => {
                 newNotif.remove()
                 showNotif(r)
+                if (!data.read){
+                    notificationBadge.text(parseInt(notificationBadge.text())-1)
+                }
             })
         } catch (e) {
             console.error(e);
